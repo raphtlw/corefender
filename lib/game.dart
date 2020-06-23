660f5965deaa249c37c3a7f2c19b5337ad0b7758
Raphael Tang <raphpb1912@gmail.com>
2020-06-23 22:04:52 +0800

    Add flame engine

---
+++ lib/game.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class MainGame extends Game {
+  Size screenSize;
+
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  void update(double t) {
+    // TODO: implement update
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
-// idk
+import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+
+import 'game.dart';
+
+void main() {
+  MainGame game = MainGame();
+  runApp(game.widget);
+
+  Util flameUtil = Util();
+  flameUtil.fullScreen();
+  flameUtil.setOrientation(DeviceOrientation.landscapeLeft);
+}
